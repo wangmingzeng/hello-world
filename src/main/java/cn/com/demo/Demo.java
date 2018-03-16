2d482789de845640960dc0495d63eee5ae5da570
wangmingzeng <370023553@qq.com>
2018-03-16 16:38:31 +0800

    no message

--- src/main/java/cn/com/demo/Demo.java
+++ src/main/java/cn/com/demo/Demo.java
@@ -1,9 +1,13 @@
 package cn.com.demo;
 
-public class Demo{
-	
-	public static void main(String[] args){
-		System.out.println("111111");
-		System.out.println("2222222");
+import com.taobao.diamond.md5.MD5;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		MD5 md5 = MD5.getInstance();
+		String pwd = md5.getMD5String(md5.getMD5String("abc123") + "hTxoTN");
+		System.out.println(pwd);
 	}
+
 }
